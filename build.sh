194e2a54d5b69f93da871bf2360537bdc3dd333c
Tehuel <torresbaldi.tehuel@gmail.com>
2017-01-19 16:22:24 -0300

    modifico scripts, separo build de test

--- build.sh
+++ build.sh
@@ -3,15 +3,3 @@
 # concatenate every prg file into one new prg
 cat prg/globals.prg prg/functions.prg prg/functions-debug.prg prg/scroll.prg > tilescroll.prg
 
-# build the test program
-cd 'test/'
-bgdc 'test.prg'
-#bgdi 'test.dcb'
-
-# invert output status of bgdc
-if [ $? -eq 1 ]
-then
-  exit 0
-else
-  exit 1
-fi
